d3deb0688b7b8763f806f6108f7396a169edf218
wasabi <openpw@icloud.com>
2026-03-06 23:33:14 +0300

    UZO-0101: добавил Entity

--- src/main/java/ru/javaops/cloudjava/menuservice/MenuServiceApplication.java
+++ src/main/java/ru/javaops/cloudjava/menuservice/MenuServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MenuServiceApplication {
 
-	  public static void main(String[] args) {
-		    SpringApplication.run(MenuServiceApplication.class, args);
-	  }
+  public static void main(final String[] args) {
+    SpringApplication.run(MenuServiceApplication.class, args);
+  }
 
 }

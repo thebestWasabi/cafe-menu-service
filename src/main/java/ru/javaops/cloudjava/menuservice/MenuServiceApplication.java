67f80427d5563146d79b30d6ce4ecf6b2332b5d1
wasabi <openpw@icloud.com>
2026-03-07 02:54:01 +0300

    1_data_layer_2_HWD1

--- src/main/java/ru/javaops/cloudjava/menuservice/MenuServiceApplication.java
+++ src/main/java/ru/javaops/cloudjava/menuservice/MenuServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MenuServiceApplication {
 
 	  public static void main(String[] args) {
-		    SpringApplication.run(MenuServiceApplication.class, args);
+        SpringApplication.run(MenuServiceApplication.class, args);
 	  }
 
 }
